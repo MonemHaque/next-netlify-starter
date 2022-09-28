7f9139f417a9eafe403cb27bbdb7e5e9d1fffdab
Monem Haque <mascot@gmail.com>
2022-09-28 16:01:25 +0000

    First Iteration

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,13 @@
+import styles from './Header.module.css'
+
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+    <div className={styles.topnav}>
+      <h1>{title}</h1>
+      <a className={styles.active} href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
+  )
 }
